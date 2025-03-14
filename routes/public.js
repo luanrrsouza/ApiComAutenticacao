6cf5bc41753aaddfb7171b8fb134ae630d809bed
luanrrouza <luanrrdsouza@gmail.com>
2025-03-13 22:09:29 -0300

    Primeira versão da API com autenticação JWT

---
+++ routes/public.js
@@ -0,0 +1,60 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// Cadastro de usuários
+router.post("/cadastro", async (req, res) => {
+  try {
+    const user = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(user.password, salt);
+
+    const userDB = await prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.name,
+        password: hashPassword,
+      },
+    });
+
+    res.status(201).json(userDB);
+  } catch (err) {
+    res.status(500).json({ message: "Erro no servidor, tente novamente" });
+  }
+});
+
+// Login
+
+router.post("/login", async (req, res) => {
+  try {
+    const userInfo = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email: userInfo.email },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    const isMatch = await bcrypt.compare(userInfo.password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Senha inválida!" });
+    }
+
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "1d" });
+
+    res.status(200).json(token);
+  } catch (err) {
+    res.status(500).json({ message: "Erro no servidor, tente novamente" });
+  }
+});
+
+export default router;
